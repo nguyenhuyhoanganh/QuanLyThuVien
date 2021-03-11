a9d18a3786cb002d5c87712f144751dd46197f3c
SCHyperion <79067994+SCHyperion@users.noreply.github.com>
2021-03-11 09:53:52 +0700

    Create fHelp

---
+++ QuanLyThuVien/WindowsFormsApp1/fHelp.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class fHelp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeview_Display = new System.Windows.Forms.TreeView();
+            this.richtextbox_Display = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // treeview_Display
+            // 
+            this.treeview_Display.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeview_Display.Location = new System.Drawing.Point(0, 0);
+            this.treeview_Display.Name = "treeview_Display";
+            this.treeview_Display.Size = new System.Drawing.Size(230, 598);
+            this.treeview_Display.TabIndex = 0;
+            // 
+            // richtextbox_Display
+            // 
+            this.richtextbox_Display.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richtextbox_Display.Location = new System.Drawing.Point(230, 0);
+            this.richtextbox_Display.Name = "richtextbox_Display";
+            this.richtextbox_Display.Size = new System.Drawing.Size(694, 598);
+            this.richtextbox_Display.TabIndex = 1;
+            this.richtextbox_Display.Text = "";
+            // 
+            // fHelp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 598);
+            this.Controls.Add(this.richtextbox_Display);
+            this.Controls.Add(this.treeview_Display);
+            this.Name = "fHelp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fHelp";
+            this.Load += new System.EventHandler(this.fHelp_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeview_Display;
+        private System.Windows.Forms.RichTextBox richtextbox_Display;
+    }
+}
---
+++ QuanLyThuVien/WindowsFormsApp1/fHelp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class fHelp : Form
+    {
+        public fHelp()
+        {
+            InitializeComponent();
+        }
+
+        private void fHelp_Load(object sender, EventArgs e)
+        {
+            richtextbox_Display.Text = File.ReadAllText(@"HelpResources\test.txt");
+
+            Clipboard.SetImage(Image.FromFile(@"HelpResources\test.jpg"));
+            richtextbox_Display.AppendText("\n");
+            richtextbox_Display.Paste();
+        }
+    }
+}
