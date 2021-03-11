2a758704f9d22424634b435db131dd3e04267cc1
SCHyperion <79067994+SCHyperion@users.noreply.github.com>
2021-03-11 10:10:46 +0700

    update

--- QuanLyThuVien/WindowsFormsApp1/fHelp.Designer.cs
+++ QuanLyThuVien/WindowsFormsApp1/fHelp.Designer.cs
@@ -37,15 +37,16 @@
             this.treeview_Display.Dock = System.Windows.Forms.DockStyle.Left;
             this.treeview_Display.Location = new System.Drawing.Point(0, 0);
             this.treeview_Display.Name = "treeview_Display";
-            this.treeview_Display.Size = new System.Drawing.Size(230, 598);
+            this.treeview_Display.Size = new System.Drawing.Size(230, 531);
             this.treeview_Display.TabIndex = 0;
             // 
             // richtextbox_Display
             // 
             this.richtextbox_Display.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richtextbox_Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richtextbox_Display.Location = new System.Drawing.Point(230, 0);
             this.richtextbox_Display.Name = "richtextbox_Display";
-            this.richtextbox_Display.Size = new System.Drawing.Size(694, 598);
+            this.richtextbox_Display.Size = new System.Drawing.Size(617, 531);
             this.richtextbox_Display.TabIndex = 1;
             this.richtextbox_Display.Text = "";
             // 
@@ -53,7 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(924, 598);
+            this.ClientSize = new System.Drawing.Size(847, 531);
             this.Controls.Add(this.richtextbox_Display);
             this.Controls.Add(this.treeview_Display);
             this.Name = "fHelp";
