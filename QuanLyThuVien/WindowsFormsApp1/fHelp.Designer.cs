1b4c83b722993d2d9252786fac5a3ed329dae261
SCHyperion <79067994+SCHyperion@users.noreply.github.com>
2021-03-12 15:38:36 +0700

    update fHelp

--- QuanLyThuVien/WindowsFormsApp1/fHelp.Designer.cs
+++ QuanLyThuVien/WindowsFormsApp1/fHelp.Designer.cs
@@ -35,18 +35,20 @@
             // treeview_Display
             // 
             this.treeview_Display.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeview_Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.treeview_Display.Location = new System.Drawing.Point(0, 0);
             this.treeview_Display.Name = "treeview_Display";
-            this.treeview_Display.Size = new System.Drawing.Size(230, 531);
+            this.treeview_Display.Size = new System.Drawing.Size(224, 547);
             this.treeview_Display.TabIndex = 0;
+            this.treeview_Display.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeview_Display_AfterSelect);
             // 
             // richtextbox_Display
             // 
             this.richtextbox_Display.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richtextbox_Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richtextbox_Display.Location = new System.Drawing.Point(230, 0);
+            this.richtextbox_Display.Location = new System.Drawing.Point(224, 0);
             this.richtextbox_Display.Name = "richtextbox_Display";
-            this.richtextbox_Display.Size = new System.Drawing.Size(617, 531);
+            this.richtextbox_Display.Size = new System.Drawing.Size(623, 547);
             this.richtextbox_Display.TabIndex = 1;
             this.richtextbox_Display.Text = "";
             // 
@@ -54,12 +56,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 531);
+            this.ClientSize = new System.Drawing.Size(847, 547);
             this.Controls.Add(this.richtextbox_Display);
             this.Controls.Add(this.treeview_Display);
             this.Name = "fHelp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "fHelp";
+            this.Text = "Help";
             this.Load += new System.EventHandler(this.fHelp_Load);
             this.ResumeLayout(false);
 
